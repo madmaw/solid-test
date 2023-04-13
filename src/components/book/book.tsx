cd6f4cb132a950fdc059adc40058bf667487f511
Chris Glover <chris.glover@gmail.com>
2023-04-13 10:17:52 +1000

    positioning

--- src/components/book/book.tsx
+++ src/components/book/book.tsx
@@ -31,9 +31,7 @@ export function Book(props: {
     <div class={styles.container}>
       <div class={styles.book}>
         {/* Back half */}
-        <Page z={-0.5 - FAKE_PAGE_COUNT / 2} style={{
-          background: 'black'
-        }} />
+        <Page z={-0.5 - FAKE_PAGE_COUNT / 2}/>
 
         <Index each={[...Array(FAKE_PAGE_COUNT / 2).keys()]}>{(page) =>
           // `FAKE_PAGE_COUNT / 2 - 1` is just so DOM order matches physical order but its pretty ugly, can probably just use `z-index`
@@ -62,11 +60,7 @@ export function Book(props: {
             <Page z={1.5 + page()} />
           }</Index>
 
-          <Page z={0.5 + FAKE_PAGE_COUNT / 2} onClick={props.onClickCover} style={{
-            background: 'black',
-            'text-align': 'center',
-            color: 'white'
-          }}>
+          <Page z={0.5 + FAKE_PAGE_COUNT / 2} onClick={props.onClickCover}>
             <h1>GRAND TERMINUS</h1>
           </Page>
         </div>
